1b41b31c6a94c0a57a4a6e37e717e8f10a579239
Matt Sickler <imMute+github@msk4.com>
2012-07-02 20:04:34 -0500

    Barebones toplevel.

---
+++ Aldec/compile/top_level.vhd
@@ -0,0 +1,89 @@
+-------------------------------------------------------------------------------
+--
+-- Title       : No Title
+-- Design      : top_level
+-- Author      : 
+-- Company     : 
+--
+-------------------------------------------------------------------------------
+--
+-- File        : U:\workspace\nexysterm\Aldec\compile\top_level.vhd
+-- Generated   : Mon Jul  2 20:01:25 2012
+-- From        : U:\workspace\nexysterm\Aldec\src\top_level.bde
+-- By          : Bde2Vhdl ver. 2.6
+--
+-------------------------------------------------------------------------------
+--
+-- Description : 
+--
+-------------------------------------------------------------------------------
+-- Design unit header --
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+
+entity top_level is
+  port(
+       i_board_clk : in STD_LOGIC;
+       i_ps2d : in STD_LOGIC;
+       i_serial_rx : in STD_LOGIC;
+       i_button : in STD_LOGIC_VECTOR(3 downto 0);
+       i_switch : in STD_LOGIC_VECTOR(7 downto 0);
+       o_ps2c : out STD_LOGIC;
+       o_serial_tx : out STD_LOGIC;
+       o_vga_hsync : out STD_LOGIC;
+       o_vga_vsync : out STD_LOGIC;
+       o_led : out STD_LOGIC_VECTOR(7 downto 0);
+       o_ssd_an : out STD_LOGIC_VECTOR(3 downto 0);
+       o_ssd_seg : out STD_LOGIC_VECTOR(7 downto 0);
+       o_vga_blu : out STD_LOGIC_VECTOR(1 downto 0);
+       o_vga_grn : out STD_LOGIC_VECTOR(2 downto 0);
+       o_vga_red : out STD_LOGIC_VECTOR(2 downto 0)
+  );
+end top_level;
+
+architecture top_level of top_level is
+
+---- Component declarations -----
+
+component CRG
+  port (
+       board_clk : in STD_LOGIC;
+       i_reset : in STD_LOGIC;
+       clk1 : out STD_LOGIC;
+       clk2 : out STD_LOGIC;
+       clk3 : out STD_LOGIC;
+       clk4 : out STD_LOGIC;
+       locked : out STD_LOGIC;
+       status : out STD_LOGIC_VECTOR(7 downto 0)
+  );
+end component;
+
+---- Signal declarations used on the diagram ----
+
+signal s_sys_clk1 : STD_LOGIC;
+signal s_sys_clk2 : STD_LOGIC;
+signal s_sys_clk3 : STD_LOGIC;
+signal s_sys_clk4 : STD_LOGIC;
+signal s_sys_dll_locked : STD_LOGIC;
+signal s_sys_reset : STD_LOGIC;
+
+begin
+
+----  Component instantiations  ----
+
+CRG_inst : CRG
+  port map(
+       board_clk => i_board_clk,
+       clk1 => s_sys_clk1,
+       clk2 => s_sys_clk2,
+       clk3 => s_sys_clk3,
+       clk4 => s_sys_clk4,
+       i_reset => '0',
+       locked => s_sys_dll_locked
+  );
+
+s_sys_reset <= not(s_sys_dll_locked);
+
+
+end top_level;
---
+++ vhdl_src/CRG.vhd
@@ -0,0 +1,95 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    23:06:45 07/01/2012 
+-- Design Name: 
+-- Module Name:    CRG - behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+--use IEEE.NUMERIC_STD.ALL;
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity CRG is
+    Port (
+        board_clk : in  std_logic;
+        i_reset : in  std_logic;
+        
+        clk1 : out std_logic; -- clk * 1
+        clk2 : out std_logic; -- clk / 2
+        clk3 : out std_logic; -- clk * 2
+        clk4 : out std_logic; -- clk * 4
+        status : out std_logic_vector(7 downto 0);
+        locked : out std_logic
+    );
+end CRG;
+
+architecture behavioral of CRG is
+    signal CLKIN_IBUFG : std_logic;
+    signal CLKDV_BUF, CLKFX_BUF, CLK0_BUF, CLK2X_BUF : std_logic;
+    signal CLK0_IBUF : std_logic;
+begin
+
+CLKIN_IBUFG_INST : IBUFG  port map (I => board_clk, O => CLKIN_IBUFG);
+
+DCM_SP_INST : DCM_SP
+    generic map(
+        CLK_FEEDBACK => "1X",
+        CLKDV_DIVIDE => 2.0,
+        CLKFX_DIVIDE => 1,
+        CLKFX_MULTIPLY => 4,
+        CLKIN_DIVIDE_BY_2 => FALSE,
+        CLKIN_PERIOD => 20.000,
+        CLKOUT_PHASE_SHIFT => "NONE",
+        DESKEW_ADJUST => "SYSTEM_SYNCHRONOUS",
+        DFS_FREQUENCY_MODE => "LOW",
+        DLL_FREQUENCY_MODE => "LOW",
+        DUTY_CYCLE_CORRECTION => TRUE,
+        FACTORY_JF => x"C080",
+        PHASE_SHIFT => 0,
+        STARTUP_WAIT => TRUE
+    )
+    port map (
+        CLKFB => CLK0_IBUF,
+        CLKIN => CLKIN_IBUFG,
+        DSSEN => '0',
+        PSCLK => '0',
+        PSEN => '0',
+        PSINCDEC => '0',
+        RST => i_reset,
+        CLKDV => CLKDV_BUF,
+        CLKFX => CLKFX_BUF,
+        CLKFX180 => open,
+        CLK0 => CLK0_BUF,
+        CLK2X => CLK2X_BUF,
+        CLK2X180 => open,
+        CLK90 => open,
+        CLK180 => open,
+        CLK270 => open,
+        LOCKED => locked,
+        PSDONE => open,
+        STATUS(7 downto 0) => status(7 downto 0)
+    );
+
+CLK0_BUFG_INST  : BUFG  port map (I => CLK0_BUF,  O => CLK0_IBUF);
+clk1 <= CLK0_IBUF;
+CLKDV_BUFG_INST : BUFG  port map (I => CLKDV_BUF, O => clk2);
+CLK2X_BUFG_INST : BUFG  port map (I => CLK2X_BUF, O => clk3);
+CLKFX_BUFG_INST : BUFG  port map (I => CLKFX_BUF, O => clk4);
+
+
+end behavioral;
+
