a394327e11f0b7f66d83098a09de4b340175ded2
Matt Sickler <imMute+github@msk4.com>
2013-11-02 16:24:43 -0500

    Modified toplevel to use the new blocks and register mappings

--- vhdl_src/top_level.vhd
+++ vhdl_src/top_level.vhd
@@ -1,328 +1,254 @@
 -------------------------------------------------------------------------------
---
--- Title       : No Title
--- Design      : top_level
--- Author      : 
--- Company     : 
---
+-- Title        : NexysTerm Top-Level
+-- Design       : NexysTerm
+-- Author       : Matt "imMute" S
 -------------------------------------------------------------------------------
---
--- File        : U:\workspace\nexysterm\Aldec\compile\top_level.vhd
--- Generated   : Wed Jul  4 22:06:12 2012
--- From        : U:\workspace\nexysterm\Aldec\src\top_level.bde
--- By          : Bde2Vhdl ver. 2.6
---
--------------------------------------------------------------------------------
---
--- Description : 
---
+-- Description : Top-level block that connects all the subcomponents together
+--               and to the outside world.
 -------------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 library unisim;
 use unisim.vcomponents.all;
+use work.nexysterm_defines_pkg.all;
 
 entity top_level is
-    port(
-        i_serial_rx : in STD_LOGIC;
-        o_serial_tx : out STD_LOGIC;
-        i_Dserial_rx : in STD_LOGIC;
-        o_Dserial_tx : out STD_LOGIC;
-        i_button : in STD_LOGIC_VECTOR(3 downto 0);
-        i_switch : in STD_LOGIC_VECTOR(7 downto 0);
-        o_led : out STD_LOGIC_VECTOR(7 downto 0);
-        o_ssd_an : out STD_LOGIC_VECTOR(3 downto 0);
-        o_ssd_seg : out STD_LOGIC_VECTOR(7 downto 0);
-        o_vga_hsync : out STD_LOGIC;
-        o_vga_vsync : out STD_LOGIC;
-        o_vga_blu : out STD_LOGIC_VECTOR(1 downto 0);
-        o_vga_grn : out STD_LOGIC_VECTOR(2 downto 0);
-        o_vga_red : out STD_LOGIC_VECTOR(2 downto 0);
-        io_ps2c : inout std_logic;
-        io_ps2d : inout std_logic;
-        i_board_clk : in STD_LOGIC
+    generic (
+        GINT_NUM_SERIAL_PORTS : integer range 1 to 8 := 8
+    );
+    port (
+        -- Serial Ports
+        i_srl_rx        : in  std_logic_vector(GINT_NUM_SERIAL_PORTS-1 downto 0);
+        o_srl_tx        : out std_logic_vector(GINT_NUM_SERIAL_PORTS-1 downto 0);
+        -- Control Buttons
+        i_btn           : in std_logic_vector(3 downto 0);
+        -- Status/Debugging LEDs
+        o_led           : out std_logic_vector(7 downto 0);
+        -- SSD
+        o_ssd_seg       : out std_logic_vector(7 downto 0);
+        o_ssd_an        : out std_logic_vector(3 downto 0);
+        -- VGA
+        o_vga_hsync     : out std_logic;
+        o_vga_vsync     : out std_logic;
+        o_vga_blu       : out std_logic_vector(1 downto 0);
+        o_vga_grn       : out std_logic_vector(2 downto 0);
+        o_vga_red       : out std_logic_vector(2 downto 0);
+        -- PS2 Keyboard
+        io_ps2c         : inout std_logic;
+        io_ps2d         : inout std_logic;
+        -- Board Oscillator
+        i_board_clk     : in std_logic
     );
 end top_level;
 
-architecture top_level of top_level is
----- Constant declarations ----
-constant C_PL_SWITCH            : std_logic_vector(7 downto 0) := X"01";
-constant C_PL_BTN               : std_logic_vector(7 downto 0) := X"02";
-constant C_PL_LED               : std_logic_vector(7 downto 0) := X"03";
-constant C_PL_SSD1              : std_logic_vector(7 downto 0) := X"04";
-constant C_PL_SSD2              : std_logic_vector(7 downto 0) := X"05";
-constant C_PL_SSD3              : std_logic_vector(7 downto 0) := X"06";
-constant C_PL_SSD4              : std_logic_vector(7 downto 0) := X"07";
-constant C_PL_SRL_STATUS        : std_logic_vector(7 downto 0) := X"11";
-constant C_PL_SRL_READ          : std_logic_vector(7 downto 0) := X"12";
-constant C_PL_SRL_WRITE         : std_logic_vector(7 downto 0) := X"13";
-constant C_PL_TRAM_ADDR_HIGH    : std_logic_vector(7 downto 0) := X"14";
-constant C_PL_TRAM_ADDR_LOW     : std_logic_vector(7 downto 0) := X"15";
-constant C_PL_TRAM_DATA_COLOR   : std_logic_vector(7 downto 0) := X"16";
-constant C_PL_TRAM_DATA_CHAR    : std_logic_vector(7 downto 0) := X"17";
-constant C_PL_PS2_STATUS        : std_logic_vector(7 downto 0) := X"18";
-constant C_PL_PS2_DATA          : std_logic_vector(7 downto 0) := X"19";
-constant C_PL_DSRL_STATUS       : std_logic_vector(7 downto 0) := X"1A";
-constant C_PL_DSRL_READ         : std_logic_vector(7 downto 0) := X"1B";
-constant C_PL_DSRL_WRITE        : std_logic_vector(7 downto 0) := X"1C";
+architecture RTL of top_level is
+---- Signal Declarations ----
+--- Clocks and Resets
+    signal s_VGA_clk    : std_logic;
+    signal s_VGA_reset  : std_logic;
+    signal s_SYS_clk    : std_logic;
+    signal s_SYS_reset  : std_logic;
+--- Serial Register Interface
+    signal A_srl_control    : A_Gx8slv_t;
+    signal A_srl_status     : A_Gx8slv_t;
+    signal A_srl_bauddiv    : A_Gx16slv_t;
+    signal A_srl_rxdata     : A_Gx8slv_t;
+    signal s_srl_rxstrobe   : std_logic_vector(7 downto 0);
+    signal A_srl_txdata     : A_Gx8slv_t;
+    signal s_srl_txstrobe   : std_logic_vector(7 downto 0);
+-- VGA Register Interface
+    signal s_vga_tram_en        : std_logic;
+    signal s_vga_tram_addr      : std_logic_vector(12 downto 0);
+    signal s_vga_tram_data      : std_logic_vector(15 downto 0);
+-- PS2 Register Interface
+    signal s_ps2_rxdata         : std_logic_vector(7 downto 0);
+    signal s_ps2_rx_strobe      : std_logic;
+    signal s_ps2_txdata         : std_logic_vector(7 downto 0);
+    signal s_ps2_tx_strobe      : std_logic;
+    signal s_ps2_status         : std_logic_vector(7 downto 0);
+    signal s_ps2_control        : std_logic_vector(7 downto 0);
+-- 
+    signal s_ssd_data           : std_logic_vector(15 downto 0);
+    signal s_btn_data           : std_logic_vector(3 downto 0);
+    signal s_btn_strobe         : std_logic;
+-- KCPSM3 Program Storage
+    signal s_kc_prog_addr        : std_logic_vector(9 downto 0);
+    signal s_kc_prog_inst        : std_logic_vector(17 downto 0);
+    signal s_kc_port_id          : std_logic_vector(7 downto 0);
+    signal s_kc_rd_strobe        : std_logic;
+    signal s_kc_in_port          : std_logic_vector(7 downto 0);
+    signal s_kc_wr_strobe        : std_logic;
+    signal s_kc_out_port         : std_logic_vector(7 downto 0);
 
 
----- Signal declarations used on the diagram ----
----- Signals ---
-signal s_kc_clk         : STD_LOGIC;
-signal s_vga_clk        : STD_LOGIC;
-signal s_srl_clkx16     : STD_LOGIC;
-signal s_sys_dll_locked : STD_LOGIC;
-signal s_sys_reset      : STD_LOGIC;
-signal s_jtag_reset     : std_logic;
-signal s_kc_reset       : std_logic;
-
--- kc signals
-signal prog_addr    : STD_LOGIC_VECTOR (9 downto 0);
-signal prog_inst    : STD_LOGIC_VECTOR (17 downto 0);
-signal port_id      : STD_LOGIC_VECTOR (7 downto 0);
-signal in_port      : STD_LOGIC_VECTOR (7 downto 0);
-signal wr_strobe    : STD_LOGIC;
-signal out_port     : STD_LOGIC_VECTOR (7 downto 0);
-signal rd_strobe    : STD_LOGIC;
-
--- io stuff
-signal s_button     : std_logic_vector(7 downto 0);
-signal s_switch     : std_logic_vector(7 downto 0);
-signal s_ssd_data   : std_logic_vector(31 downto 0);
-signal s_tram_addr  : std_logic_vector(12 downto 0);
-signal s_tram_data  : std_logic_vector(15 downto 0);
-signal s_tram_wr_en : std_logic;
-signal s_srl_status     : std_logic_vector(7 downto 0);
-signal s_srl_dout       : std_logic_vector(7 downto 0);
-signal s_srl_din        : std_logic_vector(7 downto 0);
-signal s_srl_wr_strobe  : std_logic;
-signal s_srl_rd_strobe  : std_logic;
-signal s_Dsrl_status     : std_logic_vector(7 downto 0);
-signal s_Dsrl_dout       : std_logic_vector(7 downto 0);
-signal s_Dsrl_din        : std_logic_vector(7 downto 0);
-signal s_Dsrl_wr_strobe  : std_logic;
-signal s_Dsrl_rd_strobe  : std_logic;
-signal s_ps2_dout       : std_logic_vector(7 downto 0);
-signal s_ps2_drdy       : std_logic;
-signal s_ps2_rd_strobe  : std_logic;
 
 begin
 -------------------
---   CRG isntance
+--   CRG instance
 -------------------
-CRG_inst : entity CRG
-    generic map ( G_BAUD_DIVIDER => 651 )
+CRG_i : entity CRG
     port map (
-        board_clk => i_board_clk,
-        i_reset => '0',
-        kc_clk => s_kc_clk,
-        locked => s_sys_dll_locked,
-        srl_clkx16 => s_srl_clkx16,
-        vga_clk => s_vga_clk
+        i_board_clk     => i_board_clk,
+        i_async_reset   => '0',
+        o_VGA_clk       => s_VGA_clk,
+        o_VGA_reset     => s_VGA_reset,
+        o_SYS_clk       => s_SYS_clk,
+        o_SYS_reset     => s_SYS_reset
     );
-s_sys_reset <= not s_sys_dll_locked;
+
+-------------------
+--   Serial Port PHY instance
+-------------------
+SRL_loop :
+for I in 1 to GINT_NUM_SERIAL_PORTS generate
+
+srl_phy_i : entity Serial_UART_PHY
+    port map (
+        i_clk          => s_SYS_clk,
+        i_reset        => s_SYS_reset,
+        i_serial_rx    => i_srl_rx(I-1),
+        o_serial_tx    => o_srl_tx(I-1),
+        i_control      => A_srl_control(I-1),
+        o_status       => A_srl_status(I-1),
+        i_baud_divisor => A_srl_bauddiv(I-1),
+        o_data_rx           => A_srl_rxdata(I-1),
+        i_data_rx_strobe    => s_srl_rxstrobe(I-1),
+        i_data_tx           => A_srl_txdata(I-1),
+        i_data_tx_strobe    => s_srl_txstrobe(I-1)
+    );
+
+end generate;
+
+
+-------------------
+--   VGA Toplevel
+-------------------
+VGA_i : entity VGA_top
+    port map (
+        -- KC's Port
+        i_KC_tram_clk   => s_SYS_clk,
+        i_KC_tram_en    => s_vga_tram_en,
+        i_KC_tram_addr  => s_vga_tram_addr,
+        i_KC_tram_data  => s_vga_tram_data,
+        -- VGA's Port
+        i_clk       => s_VGA_clk,
+        i_reset     => s_VGA_reset,
+        o_vga_hsync => o_vga_hsync,
+        o_vga_vsync => o_vga_vsync,
+        o_vga_blu   => o_vga_blu,
+        o_vga_grn   => o_vga_grn,
+        o_vga_red   => o_vga_red
+    );
+
+
+-------------------
+--   PS2 Interface
+-------------------
+ps2interface_inst : entity ps2interface_wrapper
+    port map (
+        io_ps2c         => io_ps2c,
+        io_ps2d         => io_ps2d,
+        
+        i_clk           => s_SYS_clk,
+        i_reset         => s_SYS_reset,
+        o_ps2_rxdata    => s_ps2_rxdata,
+        i_ps2_rx_strobe => s_ps2_rx_strobe,
+        i_ps2_txdata    => s_ps2_txdata,
+        i_ps2_tx_strobe => s_ps2_tx_strobe,
+        o_ps2_status    => s_ps2_status,
+        i_ps2_control   => s_ps2_control
+    );
 
 -------------------
 --   KCPSM3 and program ROM instances
 -------------------
-pico : entity kcpsm3
+pico_i : entity kcpsm3
     port map (
-        address => prog_addr,
-        clk => s_kc_clk,
-        in_port => in_port,
-        instruction => prog_inst,
-        interrupt => '0',
-        interrupt_ack => open,
-        out_port => out_port,
-        port_id => port_id,
-        read_strobe => rd_strobe,
-        reset => s_sys_reset,
-        write_strobe => wr_strobe
+        clk             => s_SYS_clk,
+        reset           => s_SYS_reset,
+        
+        address         => s_kc_prog_addr,
+        instruction     => s_kc_prog_inst,
+        
+        interrupt       => '0',
+        interrupt_ack   => open,
+        
+        port_id         => s_kc_port_id,
+        read_strobe     => s_kc_rd_strobe,
+        in_port         => s_kc_in_port,
+        write_strobe    => s_kc_wr_strobe,
+        out_port        => s_kc_out_port
     );
-prog_rom : entity nterm
---    port map (
---        address => prog_addr,
---        clk => s_kc_clk,
---        instruction => prog_inst
---    );
-prog_rom_jtag : nterm_jtag
+prog_rom_i : entity nterm
     port map (
-        address => prog_addr,
-        clk => s_kc_clk,
-        instruction => prog_inst,
-        proc_reset => s_jtag_reset
+        clk         => s_SYS_clk,
+        address     => s_kc_prog_addr,
+        instruction => s_kc_prog_inst
     );
 
-s_kc_reset <= s_sys_reset or s_jtag_reset;
+ssd_i : entity SSD_Hex
+    port map (
+        clk         => s_SYS_clk, 
+        data        => s_ssd_data,
+        segments    => o_ssd_seg,
+        anodes      => o_ssd_an
+    );
 
+btn_debounce_i : entity grp_debouncer
+    generic map (
+        N       => 4,
+        --CNT_VAL => 10000
+        CNT_VAL => 500000
+    )
+    port map (
+        clk_i   => s_SYS_clk,
+        data_i  => i_btn,
+        data_o  => s_btn_data,
+        strb_o  => s_btn_strobe
+    );
 -------------------
---   Input Ports
+--   KCPSM3 to Register Mapping
 -------------------
-input_stage_1: process (s_kc_clk) begin
-    if rising_edge(s_kc_clk) then
-        s_button <= "0000" & i_button;
-        s_switch <= i_switch;
-    end if;
-end process;
-
-input_stage_2: process (s_kc_clk) begin
-    if rising_edge(s_kc_clk) then
-        case (port_id) is
-            when C_PL_SWITCH =>
-                in_port <= s_switch;
-            when C_PL_BTN =>
-                in_port <= s_button;
-            when C_PL_SRL_STATUS =>
-                in_port <= s_srl_status;
-            when C_PL_SRL_READ =>
-                in_port <= s_srl_dout;
-            when C_PL_PS2_STATUS =>
-                in_port <= "0000000" & s_ps2_drdy;
-            when C_PL_PS2_DATA =>
-                in_port <= s_ps2_dout;
-            when C_PL_DSRL_STATUS =>
-                in_port <= s_Dsrl_status;
-            when C_PL_DSRL_READ =>
-                in_port <= s_Dsrl_dout;
-            when others =>
-                in_port <= "XXXXXXXX";
-        end case;
-    end if;
-end process;
-s_srl_rd_strobe <= '1' when port_id=C_PL_SRL_READ and rd_strobe='1' else '0';
-s_Dsrl_rd_strobe <= '1' when port_id=C_PL_DSRL_READ and rd_strobe='1' else '0';
-s_ps2_rd_strobe <= '1' when port_id=C_PL_PS2_DATA and rd_strobe='1' else '0';
-
--------------------
---   Output Ports
--------------------
-output_stage_1: process (s_kc_clk) begin
-    if rising_edge(s_kc_clk) then
-        s_srl_wr_strobe <= '0';
-        s_Dsrl_wr_strobe <= '0';
-        if wr_strobe='1' then
-            case (port_id) is
-                when C_PL_LED =>
-                    o_led <= out_port;
-                when C_PL_SSD1 =>
-                    s_ssd_data(7 downto 0) <= out_port;
-                when C_PL_SSD2 =>
-                    s_ssd_data(15 downto 8) <= out_port;
-                when C_PL_SSD3 =>
-                    s_ssd_data(23 downto 16) <= out_port;
-                when C_PL_SSD4 =>
-                    s_ssd_data(31 downto 24) <= out_port;
-                when C_PL_TRAM_ADDR_HIGH =>
-                    s_tram_addr(12 downto 8) <= out_port(4 downto 0);
-                when C_PL_TRAM_ADDR_LOW =>
-                    s_tram_addr(7 downto 0) <= out_port;
-                when C_PL_TRAM_DATA_COLOR =>
-                    s_tram_data(15 downto 8) <= out_port;
-                when C_PL_TRAM_DATA_CHAR =>
-                    s_tram_data(7 downto 0) <= out_port;
-                when C_PL_SRL_WRITE =>
-                    s_srl_din <= out_port;
-                    s_srl_wr_strobe <= '1';
-                when C_PL_DSRL_WRITE =>
-                    s_Dsrl_din <= out_port;
-                    s_Dsrl_wr_strobe <= '1';
-                when others => null;
-            end case;
-        end if;
-    end if;
-end process;
---s_srl_wr_strobe <= '1' when port_id=C_PL_SRL_WRITE and wr_strobe='1' else '0';
-s_tram_wr_en    <= '1' when port_id=C_PL_TRAM_DATA_CHAR and wr_strobe='1' else '0';
-
-
--------------------
---   Subcomponent Instances
--------------------
-vga_top_inst : entity vga_top
+regmap_i : entity KC_Register_Interface
     port map (
-        i_sys_reset => s_sys_reset,
-        i_tram_addr => s_tram_addr,
-        i_tram_clk => s_kc_clk,
-        i_tram_data => s_tram_data,
-        i_tram_en => s_tram_wr_en,
-        i_vga_clk => s_vga_clk,
-        o_vga_blu => o_vga_blu,
-        o_vga_grn => o_vga_grn,
-        o_vga_hsync => o_vga_hsync,
-        o_vga_red => o_vga_red,
-        o_vga_vsync => o_vga_vsync
+        -- KC Port
+        i_kc_port_id    => s_kc_port_id,
+        i_kc_rd_strobe  => s_kc_rd_strobe,
+        o_kc_in_port    => s_kc_in_port,
+        i_kc_wr_strobe  => s_kc_wr_strobe,
+        i_kc_out_port   => s_kc_out_port,
+        
+        -- Serial Registers
+        o_srl_control   => A_srl_control,
+        i_srl_status    => A_srl_status,
+        o_srl_bauddiv   => A_srl_bauddiv,
+        i_srl_rxdata    => A_srl_rxdata,
+        o_srl_rxstrobe  => s_srl_rxstrobe,
+        o_srl_txdata    => A_srl_txdata,
+        o_srl_txstrobe  => s_srl_txstrobe,
+        
+        -- VGA Registers
+        o_vga_tram_en   => s_vga_tram_en,
+        o_vga_tram_addr => s_vga_tram_addr,
+        o_vga_tram_data => s_vga_tram_data,
+        
+        -- PS2 Registers
+        i_ps2_rxdata    => s_ps2_rxdata,
+        o_ps2_rx_strobe => s_ps2_rx_strobe,
+        o_ps2_txdata    => s_ps2_txdata,
+        o_ps2_tx_strobe => s_ps2_tx_strobe,
+        i_ps2_status    => s_ps2_status,
+        o_ps2_control   => s_ps2_control,
+        
+        -- IO Registers
+        o_leds          => o_led,
+        i_btn           => s_btn_data,
+        o_ssd           => s_ssd_data,
+        
+        -- System Clk & Reset
+        i_clk           => s_SYS_clk,
+        i_reset         => s_SYS_reset
     );
 
-SSD_Driver_inst : entity SSD_Driver
-    port map (
-        anodes => o_ssd_an,
-        clk => s_kc_clk,
-        data => s_ssd_data,
-        segments => o_ssd_seg
-    );
-
-uart_rx_inst : entity uart_rx
-    port map (
-        clk => s_kc_clk,
-        en_16_x_baud => s_srl_clkx16,
-        reset_buffer => '0',
-        buffer_data_present => s_srl_status(0),
-        buffer_half_full => s_srl_status(1),
-        buffer_full => s_srl_status(2),
-        data_out => s_srl_dout,
-        read_buffer => s_srl_rd_strobe,
-        serial_in => i_serial_rx
-    );
-
-uart_tx_inst : entity uart_tx
-    port map (
-        clk => s_kc_clk,
-        en_16_x_baud => s_srl_clkx16,
-        reset_buffer => '0',
-        buffer_half_full => s_srl_status(4),
-        buffer_full => s_srl_status(5),
-        data_in => s_srl_din,
-        write_buffer => s_srl_wr_strobe,
-        serial_out => o_serial_tx
-    );
-    
-
-debug_uart_rx_inst : entity uart_rx
-    port map (
-        clk => s_kc_clk,
-        en_16_x_baud => s_srl_clkx16,
-        reset_buffer => '0',
-        buffer_data_present => s_Dsrl_status(0),
-        buffer_half_full => s_Dsrl_status(1),
-        buffer_full => s_Dsrl_status(2),
-        data_out => s_Dsrl_dout,
-        read_buffer => s_Dsrl_rd_strobe,
-        serial_in => i_Dserial_rx
-    );
-
-debug_uart_tx_inst : entity uart_tx
-    port map (
-        clk => s_kc_clk,
-        en_16_x_baud => s_srl_clkx16,
-        reset_buffer => '0',
-        buffer_half_full => s_Dsrl_status(4),
-        buffer_full => s_Dsrl_status(5),
-        data_in => s_Dsrl_din,
-        write_buffer => s_Dsrl_wr_strobe,
-        serial_out => o_Dserial_tx
-    );
-
-ps2interface_inst : entity ps2interface_wrapper
-    port map (
-        ps2_clk   => io_ps2c,
-        ps2_data  => io_ps2d,
-        clk       => s_kc_clk,
-        rst       => s_sys_reset,
-        rx_rdy    => s_ps2_drdy,
-        rx_data   => s_ps2_dout,
-        rx_strobe => s_ps2_rd_strobe
-    );
-
-end top_level;
+end architecture;
